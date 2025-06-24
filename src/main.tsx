a9753e9915f88837673a5d169cfc9cce17cb2443
Bodhesh-meher <kumarbodhesh123@gmail.com>
2025-06-24 10:43:04 +0530

    updating main file

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,10 @@ import './index.css'
 import App from './App.tsx'
 import { Toaster } from 'react-hot-toast';
 
-//import Doc from './Doc'
-//import Newapp from './Newapp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Toaster position="top-center" />
-    {/* <Doc name='Bodhesh' age={22}/> */}
-    {/* <Newapp/> */}
   </StrictMode>,
 )
